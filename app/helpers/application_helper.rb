3bb1bef6c7d27c737455f2392d2e5e905e88be3a
Cataline Baert <snuf_kin@yahoo.com>
2022-01-17 18:23:52 +0100

    Add title functionality for project show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
+# Methods that are (mostly) used in the Rails views to share reusable code
 module ApplicationHelper
+  def title(*parts)
+    return if parts.empty?
+
+    content_for :title do
+      (parts << 'Ticketee').join(' - ')
+    end
+  end
 end
--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.feature 'Users can create new projects', type: :feature do
@@ -6,10 +8,16 @@ RSpec.feature 'Users can create new projects', type: :feature do
 
     click_link 'New Project'
 
-    fill_in 'Name', with: 'Subline Text 3'
+    fill_in 'Name', with: 'Sublime Text 3'
     fill_in 'Description', with: 'A text editor for everyone'
     click_button 'Create Project'
 
     expect(page).to have_text('Project has been created.')
+
+    project = Project.find_by(name: 'Sublime Text 3')
+    expect(page.current_url).to eq project_url(project)
+
+    title = 'Sublime Text 3 - Projects - Ticketee'
+    expect(page).to have_title title
   end
 end

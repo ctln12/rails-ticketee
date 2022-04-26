8167c1ec5b4aad335dcb387ec3018ba50f686b40
Cataline Baert <snuf_kin@yahoo.com>
2022-04-26 13:56:09 +0200

    Cull tickets when project gets destroyed

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class Project < ApplicationRecord
-  has_many :tickets
+  has_many :tickets, dependent: :delete_all
+  # Other possible options :destroy, :nullify, :restrict_with_error, :restrict_with_exception
 
   validates :name, presence: true
 end
